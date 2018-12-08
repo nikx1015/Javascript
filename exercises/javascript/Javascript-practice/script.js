bf558d24cbb805120049191ea929980807e5291c
nikx1015 <nikx1015@gmail.com>
2018-12-08 11:57:24 -0500

    added javascript

---
+++ exercises/javascript/Javascript-practice/script.js
@@ -0,0 +1,19 @@
+var nikkiObject = {
+firstName: "Nikki",
+lastName: "Ash",
+age: 31,
+likesTravel: true,
+}
+
+console.log(nikkiObject);
+
+var movieArray = ["Halloween", "Nightmare Before Christmas"];
+console.log(movieArray[0]);
+
+var williamGrades = [62, 97, 99, 85, 73, 97];
+console.log("what are the grades?", williamGrades[0]);
+console.log("what is the grade", williamGrades[5]);
+console.log(williamGrades.length);
+
+var averageGrade = 
+console.log(averageGrade);
---
+++ exercises/javascript/dom-js/script.js
@@ -0,0 +1,46 @@
+var movieObject = {
+    title: "Star Wars: A New Hope",
+    genre: "Science Fiction",
+    releaseDate: "May 25, 1977"
+  }
+
+var titleContainer = document.querySelector("#title");
+var genreContainer = document.querySelector("#genre");
+var releaseContainer = document.querySelector("#releaseDate");
+
+titleContainer.innerHTML = movieObject.title;
+genreContainer.innerHTML = movieObject.genre;
+releaseContainer.innerHTML = movieObject.releaseDate;
+
+var movieSchedule = [
+    {
+      title: "Ralph Breaks the Internet",
+      rating: "PG",
+      poster: "https://lumiere-a.akamaihd.net/v1/images/r_ralphbreakstheinternet_header_ddt-17403_08ef6d92.jpeg?region=0,0,1024,1274&optimize=true"
+    }, 
+    {
+      title: "The Grinch",
+      rating: "G",
+      poster: "https://images-na.ssl-images-amazon.com/images/I/71vgX4VFdiL._SY679_.jpg"
+    },
+    {
+      title: "Fantastic Beasts: The Crimes of Grindlewald",
+      rating: "PG-13",
+      poster: "https://images-na.ssl-images-amazon.com/images/I/716fk%2BBZ-NL._SY606_.jpg"
+    }
+] 
+    var titleContainer = document.querySelector("#title");
+    var ratingContainer = document.querySelector("#rating");
+    var posterContainer = document.querySelector("#poster");
+
+
+    titleContainer.innerHTML = movieSchedule.title;
+    ratingContainer.innerHTML = movieSchedule.rating;
+    posterContainer.innerHTML = movieSchedule.moviePoster;
+
+  var htmlString = '
+    <h1 class="movieTitle">${movieSchedule.name}</h1>;
+    <h2 class="movieRating">${movieSchedule.rating}</h2>;
+    <h3 class="moviePoster">${movieSchedule.poster}</h3>;
+    <img class="movieImage" src=${movieSchedule.imageUrl} alt="Movie Poster"/>;
+    `
